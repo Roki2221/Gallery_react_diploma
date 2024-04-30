af71032d0a0ca2aab1bdb76141c98f2dc3c6c1d3
Roki2221 <100676027+Roki2221@users.noreply.github.com>
2024-04-30 14:46:06 +0300

    start

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <container>
+        <img src="/img/mt-2138-home-logo.png" alt="exhibita" />
+        <div></div>
+        <div></div>
+      </container>
+    </header>
+  );
+}
+
+export default Header;
