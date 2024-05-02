b1e8297d1905a3028d7011d8b106eea2c0c71592
Roki2221 <100676027+Roki2221@users.noreply.github.com>
2024-05-02 15:39:28 +0300

    artists container

--- src/components/Artists/Artists.jsx
+++ src/components/Artists/Artists.jsx
@@ -4,38 +4,47 @@ import art2 from '../../img/mt-2138-home-img-4s.jpg';
 import art3 from '../../img/mt-2138-home-img-5s.jpg';
 import art4 from '../../img/mt-2138-home-img-6s.jpg';
 import CSS from './Artists.module.css';
+import { Link } from 'react-router-dom';
 function Artists() {
   return (
     <div className={CSS.artists_container}>
       <h2>Artists</h2>
-      <div className={CSS.artist_item}>
-        <img src={art1} alt="" />
-        <div>
+      <Link className={CSS.artist_item} to="/events">
+        <div className={CSS.artist_img_container}>
+          <img className={CSS.artist_img} src={art1} alt="" />
+        </div>
+        <div style={{ paddingBottom: '16px' }}>
           <h3>Isabelle Bellagio</h3>
           <p>Abstract Art</p>
         </div>
-      </div>
-      <div className={CSS.artist_item}>
-        <img src={art2} alt="" />
-        <div>
+      </Link>
+      <Link className={CSS.artist_item} to="/events">
+        <div className={CSS.artist_img_container}>
+          <img className={CSS.artist_img} src={art2} alt="" />
+        </div>
+        <div style={{ paddingBottom: '16px' }}>
           <h3>Mary Cameron</h3>
           <p>Art Installation</p>
         </div>
-      </div>
-      <div className={CSS.artist_item}>
-        <img src={art3} alt="" />
-        <div>
-          <h3>Alice Nixon</h3>
-          <p>NAture Photography</p>
+      </Link>
+      <Link className={CSS.artist_item} to="/events">
+        <div className={CSS.artist_img_container}>
+          <img className={CSS.artist_img} src={art3} alt="" />
         </div>
-      </div>
-      <div>
-        <img src={art4} alt="" />
-        <div className={CSS.artist_item}>
-          <h3>MArie Steady</h3>
+        <div style={{ paddingBottom: '16px' }}>
+          <h3>Alice Nixon</h3>
+          <p>Nature Photography</p>
+        </div>
+      </Link>
+      <Link className={CSS.artist_item} to="/events">
+        <div className={CSS.artist_img_container}>
+          <img className={CSS.artist_img} src={art4} alt="" />
+        </div>
+        <div style={{ paddingBottom: '16px' }}>
+          <h3>Marie Steady</h3>
           <p>Modern Art</p>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
