dce837fe38e9a624b24420ea7af2c8ffefe3d897
Roki2221 <100676027+Roki2221@users.noreply.github.com>
2024-05-10 02:48:19 +0300

    shedule adaptive

--- src/components/PlanningVisitSchedule/PlanningVisitSchedule.jsx
+++ src/components/PlanningVisitSchedule/PlanningVisitSchedule.jsx
@@ -6,35 +6,37 @@ import CSS from './PlanningVisitSchedule.module.css';
 import { Link } from 'react-router-dom';
 function PlanningVisitSchedule() {
   return (
-    <div className={`container ${CSS.schedule_container}`}>
-      <h2>PlanningVisitSchedule</h2>
-      <Link className={CSS.artist_item} to="/events">
-        <div className={CSS.artist_img_container}>
-          <img className={CSS.artist_img} src={art1} alt="" />
-        </div>
-        <div className={CSS.gallery_item_text}>
-          <h3>Abstract Art</h3>
-          <p>Friday, 2:00 pm - 11:00 pm</p>
-        </div>
-      </Link>
-      <Link className={CSS.artist_item} to="/events">
-        <div className={CSS.artist_img_container}>
-          <img className={CSS.artist_img} src={art2} alt="" />
-        </div>
-        <div className={CSS.gallery_item_text}>
-          <h3>Portraits</h3>
-          <p>Monday, 2:00 pm - 11:00 pm</p>
-        </div>
-      </Link>
-      <Link className={CSS.artist_item} to="/events">
-        <div className={CSS.artist_img_container}>
-          <img className={CSS.artist_img} src={art3} alt="" />
-        </div>
-        <div className={CSS.gallery_item_text}>
-          <h3>Modern Art</h3>
-          <p>Tuesday, 2:00 pm - 11:00 pm</p>
-        </div>
-      </Link>
+    <div className="container">
+      <h2 className={CSS.schedule_title}>PlanningVisitSchedule</h2>
+      <div className={CSS.schedule_container}>
+        <Link className={CSS.artist_item} to="/events">
+          <div className={CSS.artist_img_container}>
+            <img className={CSS.artist_img} src={art1} alt="" />
+          </div>
+          <div className={CSS.gallery_item_text}>
+            <h3>Abstract Art</h3>
+            <p>Friday, 2:00 pm - 11:00 pm</p>
+          </div>
+        </Link>
+        <Link className={CSS.artist_item} to="/events">
+          <div className={CSS.artist_img_container}>
+            <img className={CSS.artist_img} src={art2} alt="" />
+          </div>
+          <div className={CSS.gallery_item_text}>
+            <h3>Portraits</h3>
+            <p>Monday, 2:00 pm - 11:00 pm</p>
+          </div>
+        </Link>
+        <Link className={CSS.artist_item} to="/events">
+          <div className={CSS.artist_img_container}>
+            <img className={CSS.artist_img} src={art3} alt="" />
+          </div>
+          <div className={CSS.gallery_item_text}>
+            <h3>Modern Art</h3>
+            <p>Tuesday, 2:00 pm - 11:00 pm</p>
+          </div>
+        </Link>
+      </div>
       <Link
         style={{ textDecoration: 'none' }}
         to="/exhibitions"
