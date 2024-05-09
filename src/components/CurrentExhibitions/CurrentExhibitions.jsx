d1643c72acb8f6aec2dffa16e54af4e9453637ca
Roki2221 <100676027+Roki2221@users.noreply.github.com>
2024-05-10 02:07:56 +0300

    currentExhibitions adaptive

--- src/components/CurrentExhibitions/CurrentExhibitions.jsx
+++ src/components/CurrentExhibitions/CurrentExhibitions.jsx
@@ -5,11 +5,13 @@ import exhImg2 from '../../img/mt-2138-home-gal-img-2-s.jpg';
 import exhImg3 from '../../img/mt-2138-home-gal-img-3-s.jpg';
 import exhImg4 from '../../img/mt-2138-home-gal-img-4-s.jpg';
 import exhImg5 from '../../img/mt-2138-home-gal-img-5-s.jpg';
+import exhImg6 from '../../img/mt-2138-home-gal-img-6-s.jpg';
+
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import SimpleLightbox from 'simplelightbox';
 
 function CurrentExhibitions() {
-  const images = [exhImg1, exhImg2, exhImg3, exhImg4, exhImg5];
+  const images = [exhImg1, exhImg2, exhImg3, exhImg4, exhImg5, exhImg6];
 
   useEffect(() => {
     const lightbox = new SimpleLightbox('.gallery a', {
