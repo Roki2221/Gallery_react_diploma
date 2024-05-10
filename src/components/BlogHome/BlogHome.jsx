9cf4c08664fb82fb7c2487df0e924ab5028f0010
Roki2221 <100676027+Roki2221@users.noreply.github.com>
2024-05-10 03:12:24 +0300

    blog adaptation

--- src/components/BlogHome/BlogHome.jsx
+++ src/components/BlogHome/BlogHome.jsx
@@ -9,40 +9,48 @@ function BlogHome() {
   return (
     <div className={`container ${CSS.blog_container}`}>
       <h2 className={CSS.blog_title}>Follow Our Art Blog</h2>
-      <img src={img1} alt="" />
-      <Link>
-        <h3 className={CSS.blog_text_title}>
-          The most common mistakes when managing personal finances
-        </h3>
-      </Link>
-      <p className={CSS.blog_text}>
-        The ability to manage money competently is especially valuable quality
-        in the conditions of financial crisis, when the purchasing power of the
-        population is shrinking, inflation is rising, and currency exchange
-        rates.
-      </p>
-      <img src={img2} alt="" />
-      <Link>
-        <h3 className={CSS.blog_text_title}>Methods of the recruitment</h3>
-      </Link>
-      <p className={CSS.blog_text}>
-        Search of staff is not an easy task. According to the departmental
-        heads' of personnel management words, in order to find a personnel who
-        will correspond to the relevant customer needs and requirements, it is
-        necessary to carry out a great job.
-      </p>
-      <img src={img3} alt="" />
-      <Link>
-        <h3 className={CSS.blog_text_title}>
-          Overalls with logo as a method of advertising
-        </h3>
-      </Link>
-      <p className={CSS.blog_text}>
-        Overalls bearing the company's logo are related to economy and
-        practicality. A preference of corporate style involves a significant
-        increase of costs for development of design solutions, customized
-        tailoring, selection of necessary materials and so on.
-      </p>
+      <div className={CSS.blog_list}>
+        <div className={CSS.blog_item}>
+          <img src={img1} alt="" />
+          <Link>
+            <h3 className={CSS.blog_text_title}>
+              The most common mistakes when managing personal finances
+            </h3>
+          </Link>
+          <p className={CSS.blog_text}>
+            The ability to manage money competently is especially valuable
+            quality in the conditions of financial crisis, when the purchasing
+            power of the population is shrinking, inflation is rising, and
+            currency exchange rates.
+          </p>
+        </div>
+        <div className={CSS.blog_item}>
+          <img src={img2} alt="" />
+          <Link>
+            <h3 className={CSS.blog_text_title}>Methods of the recruitment</h3>
+          </Link>
+          <p className={CSS.blog_text}>
+            Search of staff is not an easy task. According to the departmental
+            heads' of personnel management words, in order to find a personnel
+            who will correspond to the relevant customer needs and requirements,
+            it is necessary to carry out a great job.
+          </p>
+        </div>
+        <div className={CSS.blog_item}>
+          <img src={img3} alt="" />
+          <Link>
+            <h3 className={CSS.blog_text_title}>
+              Overalls with logo as a method of advertising
+            </h3>
+          </Link>
+          <p className={CSS.blog_text}>
+            Overalls bearing the company's logo are related to economy and
+            practicality. A preference of corporate style involves a significant
+            increase of costs for development of design solutions, customized
+            tailoring, selection of necessary materials and so on.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
